f958229b9d688be0fac94e586e15613e1a334c47
flamesdev <johannpayer123@gmail.com>
2019-01-04 15:35:14 -0500

    Added color switch & minor changes
    - Added color switch.
    - Changed the fruit mode toggle key to UP ARROW from CTRL.

--- script.js
+++ script.js
@@ -23,64 +23,92 @@ window.onload = function() {
     "use strict";
     hexElement = document.getElementById("hex");
     nameElement = document.getElementById("name");
+    colors = standard_colors;
 };
 
+var colors;
+var index = 0;
 document.addEventListener("keydown", (event) => {
-    if (event.keyCode == 17) {
+    if (event.keyCode === 38) {
         fruit_mode = !fruit_mode;
-        UpdateColor();
-    } else {
-        var keyName = event.key.toLowerCase();
-        var length = hexCode.length;
-
-        if (event.keyCode == 8)
-            hexCode = hexCode.substring(0, length - 1)
-        else if (length < 6 && "0123456789abcdef".split("").includes(keyName))
-            hexCode += keyName;
-
-        UpdateColor();
-        hexElement.innerHTML = "#" + hexCode;
-        if (hexCode.length === 6)
-            document.body.style.backgroundColor = "#" + hexCode;
-        else {
-            document.body.style.backgroundColor = "black";
-            nameElement.innerHTML = "";
-        }
-
-        var rgb = Functions.hexToRGB(hexCode);
-        var color;
-        if (Functions.getAverage(rgb) / 255 >= 0.5)
-            color = "black";
+        if (fruit_mode)
+            colors = fruit_colors;
         else
-            color = "white";
-        hexElement.style.color = color;
-        nameElement.style.color = color;
+            colors = standard_colors;
+        UpdateColor();
+        index = 0;
+    }
+    if (event.keyCode === 37) {
+        if (index === 0)
+            index = colors.length - 1;
+        else
+            index--;
+        UpdateIndexColor();
+        return;
+    }
+    if (event.keyCode === 39) {
+        if (index === colors.length - 1)
+            index = 0
+        else
+            index++;
+        UpdateIndexColor();
+        return;
+    }
+    var keyName = event.key.toLowerCase();
+    var length = hexCode.length;
+
+    if (event.keyCode === 8 && hexCode.length !== 0)
+        hexCode = hexCode.substring(0, length - 1);
+    else if (length < 6 && "0123456789abcdef".split("").includes(keyName))
+        hexCode += keyName;
+
+    UpdateColor();
+    UpdateDisplay();
+});
+
+function UpdateColor() {
+    if (hexCode.length === 6) {
+        var rgb = Functions.hexToRGB(hexCode);
+        var closest = -1;
+        var displayColor = "";
+        Array.prototype.forEach.call(colors, item => {
+            var color = Functions.hexToRGB(item.Color);
+            var similarity = Math.abs(color[0] - rgb[0]) +
+                Math.abs(color[1] - rgb[1]) +
+                Math.abs(color[2] - rgb[2]);
+            if (closest === -1 || similarity < closest) {
+                closest = similarity;
+                displayColor = item.Name;
+            }
+        });
+        nameElement.innerHTML = displayColor;
+    }
+}
+
+function UpdateDisplay() {
+    hexElement.innerHTML = "#" + hexCode;
+    if (hexCode.length === 6)
+        document.body.style.backgroundColor = "#" + hexCode;
+    else {
+        document.body.style.backgroundColor = "black";
+        nameElement.innerHTML = "";
     }
 
-    function UpdateColor() {
-        if (hexCode.length === 6) {
-            var rgb = Functions.hexToRGB(hexCode);
-            var colors;
-            if (fruit_mode)
-                colors = fruit_colors;
-            else
-                colors = standard_colors;
-            var closest = -1;
-            var displayColor = "";
-            Array.prototype.forEach.call(colors, item => {
-                var color = Functions.hexToRGB(item.Color);
-                var similarity = Math.abs(color[0] - rgb[0]) +
-                    Math.abs(color[1] - rgb[1]) +
-                    Math.abs(color[2] - rgb[2]);
-                if (closest === -1 || similarity < closest) {
-                    closest = similarity;
-                    displayColor = item.Name;
-                }
-            });
-            nameElement.innerHTML = displayColor;
-        }
-    }
-});
+    var rgb = Functions.hexToRGB(hexCode);
+    var color;
+    if (Functions.getAverage(rgb) / 255 >= 0.5)
+        color = "black";
+    else
+        color = "white";
+    hexElement.style.color = color;
+    nameElement.style.color = color;
+}
+
+function UpdateIndexColor() {
+    hexCode = colors[index].Color;
+    UpdateColor();
+    UpdateDisplay();
+}
 
 class Functions {
     static hexToRGB(hex) {
